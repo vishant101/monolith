eeee12aa1991752cd610c6ce4ed9288825f8a3a4
Vishant Singh <vishant@perlego.com>
2021-08-29 19:16:58 +0100

    move home into screens folder

--- App.tsx
+++ App.tsx
@@ -1,15 +1,7 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import {Home} from './src/Views/Screens';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,7 +9,7 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
