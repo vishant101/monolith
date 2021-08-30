299e5799c6764091a3c53d8da5c59418b6237c66
Vishant Singh <vishant@perlego.com>
2021-08-30 21:40:47 +0100

    Mock api request time

--- src/Api/Transactions/index.ts
+++ src/Api/Transactions/index.ts
@@ -1,5 +1,9 @@
 import data from './data/transactions-medium.json';
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 export const getTransactions = async () => {
+  await sleep(1000);
   return data;
 };
