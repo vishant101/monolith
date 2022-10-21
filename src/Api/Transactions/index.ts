1be7d4d1d8af1ea361f10995b0dd5cad41890413
Vishant Singh <vishant@perlego.com>
2022-10-21 22:50:39 +0100

    Remove logs

--- src/Api/Transactions/index.ts
+++ src/Api/Transactions/index.ts
@@ -15,7 +15,6 @@ const TRANSACTIONS_URL = 'https://api.jsonbin.io/v3/b/63530a9965b57a31e69e43a6';
 export const getTransactions = axios
   .get(TRANSACTIONS_URL)
   .then(function (response) {
-    console.log('response', response.data.record.transactions);
     return response.data.record.transactions;
   })
   .catch(function (error) {
